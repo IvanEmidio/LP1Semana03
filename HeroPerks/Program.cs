266105fbf25f60168744ff7dbb9d95ac7345b5e8
IvanEmidio <ivan.emidio@live.com.pt>
2024-03-07 16:49:05 +0000

    Perks exercicio

---
+++ HeroPerks/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace HeroPerks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Perks playerPerks = Perks.None;
+
+            foreach (char x in args[0])
+            {
+                switch(x)
+                {
+                    case 'w':
+                        playerPerks ^= Perks.WaterBreathing;
+                        break;
+                    case 's':
+                        playerPerks ^= Perks.Stealth;
+                        break;
+                    case 'a':
+                        playerPerks ^= Perks.AutoHeal;
+                        break;
+                    case 'd':
+                        playerPerks ^= Perks.DoubleJump;
+                        break;
+                    default:
+                        Console.WriteLine("Unknown Perk!");
+                        break;
+
+                    
+                }
+
+            }
+
+            if(playerPerks == Perks.None )
+            {
+                Console.WriteLine("No Perks at all!");
+                return;
+            }
+
+            Console.WriteLine($"Player Perks: {playerPerks}");
+
+            if((playerPerks & Perks.Stealth) == Perks.Stealth && (playerPerks & Perks.DoubleJump) == Perks.DoubleJump)
+            {
+                Console.WriteLine("Silent Jumper!");
+            }
+            if ((playerPerks & Perks.AutoHeal) != Perks.AutoHeal)
+            {
+                Console.WriteLine("Not gonna make it!");
+            }
+
+            
+        }
+    }
+}
