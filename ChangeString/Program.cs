0f20a4caa8ecb3c51955c86c532b9a485a4ada14
IvanEmidio <ivan.emidio@live.com.pt>
2024-03-07 10:12:59 +0000

    Change string exercicio

---
+++ ChangeString/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ChangeString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            char a = char.Parse(Console.ReadLine());
+           
+
+            foreach (char b in s)
+            {
+                if(b == a)
+                {
+                    Console.Write("X");
+                }
+                else
+                {
+                    Console.Write(b);
+                }
+                
+            }
+            
+        }
+    }
+}
