cedf893c7b5d5995841d09c37e07432c03275429
IvanEmidio <ivan.emidio@live.com.pt>
2025-03-05 15:48:18 +0000

    Started exercise 2, still to complete

---
+++ ChangeString/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ChangeString
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("String: ");
+            string s = Console.ReadLine();
+            Console.WriteLine("Character: ");
+            string c = Console.ReadLine();
+
+            foreach (char a in s)
+            {
+                
+            }
+            Console.WriteLine(s);
+        }
+    }
+}
