67d09b30258a8d8c917baa996c6d74cd996c54ac
Ivan <ivan.emidio@live.com.pt>
2025-03-06 17:17:04 +0000

    Completed exercise ChangeString

--- ChangeString/Program.cs
+++ ChangeString/Program.cs
@@ -6,16 +6,27 @@ namespace ChangeString
     {
         private static void Main(string[] args)
         {
+            string sub = "";
+
             Console.WriteLine("String: ");
             string s = Console.ReadLine();
             Console.WriteLine("Character: ");
-            string c = Console.ReadLine();
+            char c = Console.ReadKey().KeyChar;
+            Console.WriteLine();
+
 
             foreach (char a in s)
             {
-                
+                if(a == c)
+                {
+                    sub += "x";
+                }
+                else
+                {
+                    sub += a;
+                }
             }
-            Console.WriteLine(s);
+            Console.WriteLine(sub);
         }
     }
 }
