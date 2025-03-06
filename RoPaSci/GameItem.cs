2bbc7d141e40ac9c13c13285f8a62d1625878530
Ivan <ivan.emidio@live.com.pt>
2025-03-06 20:47:54 +0000

    Completed Exercise RoPaSci

---
+++ RoPaSci/GameItem.cs
@@ -0,0 +1,10 @@
+namespace RoPaSci
+{
+    public enum GameItem
+    {
+        None = 0,
+        Rock = 1,
+        Paper = 2,
+        Scissor = 3
+    }
+}
---
+++ RoPaSci/GameStatus.cs
@@ -0,0 +1,10 @@
+namespace RoPaSci
+{
+    public enum GameStatus
+    {
+        None = 0,
+        Draw = 1,
+        Player1Wins = 2,
+        Player2Wins = 3
+    }
+}
